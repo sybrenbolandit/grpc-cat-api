7d205851061b85edec83b81fb6ea620647097421
sybrenbolandit <sybrenbolandit@gmail.com>
2020-11-23 21:37:13 +0100

    Add integration test

---
+++ src/test/java/nl/sybrenbolandit/grpc/cat/api/it/CatApiIT.java
@@ -0,0 +1,30 @@
+package nl.sybrenbolandit.grpc.cat.api.it;
+
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import nl.sybrenbolandit.proto.Cat;
+import nl.sybrenbolandit.proto.CatRequest;
+import nl.sybrenbolandit.proto.CatServiceGrpc;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@MicronautTest
+public class CatApiIT {
+
+    @Inject
+    CatServiceGrpc.CatServiceBlockingStub blockingStub;
+
+    @Test
+    void testHelloWorld() {
+        final CatRequest request = CatRequest.newBuilder()
+                .setChipId("92821-1231")
+                .build();
+
+        Cat result = blockingStub.getCat(request);
+
+        assertEquals("Freddy", result.getName());
+        assertEquals(5, result.getAge());
+    }
+}
